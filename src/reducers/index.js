1f970fb06db6bab714d769e3e26568d693b06abb
kimdongwon <ehddnjs8989@gmail.com>
2019-02-04 14:33:19 +0900

    divide reducer in redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,37 @@
-import * as types from "../actions/ActionTypes";
+// import * as types from "../actions/ActionTypes";
+import number from "./number";
+import color from "./color";
+import { combineReducers } from "redux";
 
-const initialState = {
-  color: "black",
-  number: 0
-};
+// const initialState = {
+//   color: "black",
+//   number: 0
+// };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case types.INCREMENT:
-      return {
-        ...state,
-        number: state.number + 1
-      };
-    case types.DECREMENT:
-      return {
-        ...state,
-        number: state.number - 1
-      };
-    case types.SET_COLOR:
-      return {
-        ...state,
-        color: action.color
-      };
-    default:
-      return state;
-  }
-}
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case types.INCREMENT:
+//       return {
+//         ...state,
+//         number: state.number + 1
+//       };
+//     case types.DECREMENT:
+//       return {
+//         ...state,
+//         number: state.number - 1
+//       };
+//     case types.SET_COLOR:
+//       return {
+//         ...state,
+//         color: action.color
+//       };
+//     default:
+//       return state;
+//   }
+// }
+const reducers = combineReducers({
+  numberData: number,
+  colorData: color
+});
 
-export default counter;
+export default reducers;
